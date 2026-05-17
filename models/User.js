26b99dd2b7e26d744269ca5219bb5338465452fa
dfsdsrgdqfgs-creator <dfsdsrgdqfgs@gmail.com>
2026-05-17 18:37:45 +0100

    models/User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+
     username:String,
+
     email:String,
+
     password:String,
+
     avatar:String
+
 });
 
 module.exports = mongoose.model("User", UserSchema);
